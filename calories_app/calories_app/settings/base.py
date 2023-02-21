7040a2cde41cf85a30aebf02af843138c9a4bf34
Raj Patel <rajp.patel@thegatewaycorp.com>
2023-02-21 15:25:07 +0530

    Update settings base file

--- calories_app/calories_app/settings/base.py
+++ calories_app/calories_app/settings/base.py
@@ -12,6 +12,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third-party installed apps
+    'rest_framework',
+    'django_extensions',
+
+    # local apps
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -82,3 +89,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
